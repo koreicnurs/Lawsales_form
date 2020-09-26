2c2797ebd7a2e138e5f8a1c292fb8498a6adbb5f
koreicnurs <koreicnurs@gmail.com>
2020-09-26 17:25:50 +0600

    stylized form(subs, buttons), add box-shadow and radius for all content

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// let radio = document.getElementsByName('gender');
+
+// for (let i=0; i<radio.length; i++) {
+//   radio[i].onchange = testRadio;
+// }
+
+// function testRadio() {
+//   console.log(this.value);
+// }
+
+// document.getElementById('save').onclick = cheackOut;
+
+// function cheackOut() {
+//   let val = document.getElementsByName('gender');
+//   for(let i=0; i<val.length; i++) {
+//     if (val[i].cheacked) {
+//       alert(val[i].value);
+//       break;
+//     }
+//   }
+// }
